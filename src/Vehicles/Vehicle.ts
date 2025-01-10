55bdcca4e47c9900be3f542245d76dd2fd7b4d59
runi95 <runar.serigstad@gmail.com>
2025-01-10 21:35:38 +0100

    Made unit readonly on Vehicle

--- src/Vehicles/Vehicle.ts
+++ src/Vehicles/Vehicle.ts
@@ -6,7 +6,7 @@ export class Vehicle {
   public readonly customData: unknown;
   public readonly upgradeMap = new Map<string, number>();
   public readonly cooldowns = new Map<string, number>();
-  public unit: Unit;
+  public readonly unit: Unit;
   public readonly weapons: string[];
 
   constructor(unit: Unit, weapons: string[]) {
