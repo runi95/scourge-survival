98f1e6b2618eefdee0fbf6c6c501c939c76a4cc8
runi95 <runar.serigstad@gmail.com>
2025-01-10 21:17:02 +0100

    Reduced AttributeBonus from 6 => 4

--- src/Vehicles/Upgrades/Common/AttributeBonus.ts
+++ src/Vehicles/Upgrades/Common/AttributeBonus.ts
@@ -8,11 +8,11 @@ export class AttributeBonus extends VehicleUpgrade {
   public readonly icon = "ReplaceableTextures/CommandButtons/BTNStatUp.blp";
   public readonly cost = 150;
   public readonly description =
-    "Increases Strength, Agility and Intelligence by +6 points.";
+    "Increases Strength, Agility and Intelligence by +4 points.";
 
   public applyUpgrade(vehicle: Vehicle): void {
-    vehicle.unit.setStrength(vehicle.unit.strength + 6, true);
-    vehicle.unit.setAgility(vehicle.unit.agility + 6, true);
-    vehicle.unit.setIntelligence(vehicle.unit.intelligence + 6, true);
+    vehicle.unit.setStrength(vehicle.unit.strength + 4, true);
+    vehicle.unit.setAgility(vehicle.unit.agility + 4, true);
+    vehicle.unit.setIntelligence(vehicle.unit.intelligence + 4, true);
   }
 }
