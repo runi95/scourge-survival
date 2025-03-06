ac6020b6e2c81248d75d8b3d7c8fdf9bb14543d5
runi95 <runar.serigstad@gmail.com>
2025-03-06 21:44:45 +0100

    Added a wave 12 spawn sound

--- src/Game/Waves/Twelve.ts
+++ src/Game/Waves/Twelve.ts
@@ -1,3 +1,5 @@
+import { Sound } from "../../../node_modules/w3ts/index";
+import { Sounds } from "../../Utility/Sounds";
 import { StrengthInNumbers } from "../CreepUpgrades/StrengthInNumbers";
 import { CREEP_TYPE, GameMap } from "../GameMap";
 import { Wave } from "./Wave";
@@ -30,6 +32,17 @@ export const TWELVE: Wave = {
   ],
   bonusUpgrades: [new StrengthInNumbers()],
   before: () => {
+    const spawnSkeletonSound = Sound.create(
+      Sounds.THEYLL_ALL_BE_MINE_IN_THE_END,
+      false,
+      false,
+      true,
+      10,
+      10,
+      "DefaultEAXON"
+    );
+    spawnSkeletonSound.start();
+
     const localPlayerId = GetPlayerId(GetLocalPlayer());
     PingMinimapEx(
       GameMap.PLAYER_AREAS[localPlayerId].maxX - 100,
--- src/Utility/Sounds.ts
+++ src/Utility/Sounds.ts
@@ -11,9 +11,11 @@ export enum Sounds {
   // Wave 9 sound
   NO_GUTS_NO_GLORY = "Units/Undead/Ghoul/GhoulPissed4.flac",
 
+  // Wave 12 sound
+  THEYLL_ALL_BE_MINE_IN_THE_END = "Sound/Units/Undead/Necromancer/NecromancerWarcry1.flac",
+
   DEATH_SHALL_CLEANSE_THE_WORLD = "Sound/Units/Undead/Acolyte/AcolytePissed2.flac",
   LET_BLOOD_DROWN_THE_WEAK = "Sound/Units/Undead/Acolyte/AcolytePissed4.flac",
-  THEYLL_ALL_BE_MINE_IN_THE_END = "Sound/Units/Undead/Necromancer/NecromancerWarcry1.flac",
   VICTORY_DRAWS_NEAR_HASTEN_THE_ATTACK = "Sound/Dialogue/UndeadExpCamp/Undead06x/L06Varimathras42.flac",
   FROST_WYRM_WAR_CRY = "Units/Undead/FrostWyrm/FrostWyrmWarcry1.flac",
 }
