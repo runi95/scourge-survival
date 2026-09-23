1e001e8354c676a25f2c2f61dbc35aa85dc39729
Runar Serigstad <runar.serigstad@gmail.com>
2026-09-23 13:19:21 +0200

    Improved random generation for `RandomNumberGenerator.ts`

--- src/Utility/RandomNumberGenerator.ts
+++ src/Utility/RandomNumberGenerator.ts
@@ -21,7 +21,7 @@ export class RandomNumberGenerator {
       return -1;
     }
 
-    return ModuloInteger(this.next(), max + 1 - min) + min;
+    return ModuloInteger(this.next() >>> 15, max + 1 - min) + min;
   }
 }
 
